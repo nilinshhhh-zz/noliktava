e50c94658c1df11d2ac330785edbcbda451bae9d
Nils Ivanovs <nils@magebit.com>
2017-06-13 22:06:57 +0300

    Fix news styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+    <link rel="stylesheet" href="<?php echo asset('css/news.css')?>" type="text/css">
     <div class="container">
         <br><br>
         <div class="row">
             @foreach($posts as $post)
-                <div class="col-md-3">
+                <div class="col-md-3 news">
                     <a href="/post/{{ $post->slug }}">
                         <img src="{{ Voyager::image( $post->image ) }}" style="width:100%">
 
